53a0cb7cd81d120b30729f5b694572e04206f90b
Kacper <kacper.jerzy.kawala@gmail.com>
2022-05-07 13:18:19 +0200

    alpha v0.3

--- app/registry.php
+++ app/registry.php
@@ -1,13 +1,19 @@
 <section>
   <form class="" action="login.php" method="post">
     <div class="col-6 col-12-xsmall">
-      <input type="login" name="login" placeholder="LOGIN">
+      <input type="text" name="name" placeholder="IMIĘ">
     </div>
     <div class="col-6 col-12-xsmall">
-      <input type="haslo" name="haslo" placeholder="HASŁO">
+      <input type="text" name="vorname" placeholder="NAZWISKO">
     </div>
     <div class="col-6 col-12-xsmall">
-      <input type="haslo" name="phaslo" placeholder="POWTÓRZ HASŁO">
+      <input type="text" name="login" placeholder="LOGIN">
+    </div>
+    <div class="col-6 col-12-xsmall">
+      <input type="password" name="password" placeholder="HASŁO">
+    </div>
+    <div class="col-6 col-12-xsmall">
+      <input type="password" name="ppassword" placeholder="POWTÓRZ HASŁO">
     </div>
     <div class="col-6 col-12-xsmall">
       <input type="email" name="email" placeholder="E-MAIL">
