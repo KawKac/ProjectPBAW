0cf98f7a904b35e86138fb46e0db816a6c972374
Kacper <kacper.jerzy.kawala@gmail.com>
2022-05-12 21:23:52 +0200

    alpha v1.8

--- app/clean.php
+++ app/clean.php
@@ -5,9 +5,9 @@
 		<title>Hyperspace by HTML5 UP</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+<?php
+foreach (glob("assets/css/*.css") as $filename) echo "<link rel='stylesheet' href='$filename'>";
+?>
 	</head>
 	<body class="is-preload">
 
@@ -40,15 +40,9 @@
 				</div>
 			</footer>
 
-			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-			<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-			<script src="https://code.jquery.com/jquery-3.5.0.js"></script>
-			<script src="assets/js/browser.min.js"></script>
-			<script src="assets/js/breakpoints.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
-			<script src="assets/js/navbarcontrol.js"></script>
+<?php
+	foreach (glob("assets/js/*.js") as $filename)	echo "<script src='$filename'></script>";
+?>
 			<script type="text/javascript">
 				$(document).ready(function(){
 					$("#intro").load("app/home.php")
