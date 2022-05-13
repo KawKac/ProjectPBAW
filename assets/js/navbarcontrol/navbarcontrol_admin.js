d55ef30843efe1ec9afe6cfb5bdb6e58953c9bea
Kacper <kacper.jerzy.kawala@gmail.com>
2022-05-13 20:41:56 +0200

    alpha v1.15

---
+++ assets/js/navbarcontrol/navbarcontrol_admin.js
@@ -0,0 +1,35 @@
+$("#0").click(function()
+{
+  $("#intro").html("");
+  $("#intro").load("app/pages/home.php");
+});
+
+$("#1").click(function()
+{
+  $("#intro").html("");
+  $("#intro").load("app/pages/delivery.php");
+});
+
+$("#2").click(function()
+{
+  $("#intro").html("");
+  $("#intro").load("app/pages/options.php");
+});
+
+$("#3").click(function()
+{
+  $("#intro").html("");
+  $("#intro").load("app/pages/work.php");
+});
+
+$("#4").click(function()
+{
+  $("#intro").html("");
+  $("#intro").load("app/pages/admintools.php");
+});
+
+$("#5").click(function()
+{
+  $("#intro").html("");
+  $("#intro").load("app/pages/database_editor.php");
+});
---
+++ assets/js/navbarcontrol/navbarcontrol_employ.js
@@ -0,0 +1,23 @@
+$("#0").click(function()
+{
+  $("#intro").html("");
+  $("#intro").load("app/pages/home.php");
+});
+
+$("#1").click(function()
+{
+  $("#intro").html("");
+  $("#intro").load("app/pages/delivery.php");
+});
+
+$("#2").click(function()
+{
+  $("#intro").html("");
+  $("#intro").load("app/pages/work.php");
+});
+
+$("#3").click(function()
+{
+  $("#intro").html("");
+  $("#intro").load("app/pages/options.php");
+});
