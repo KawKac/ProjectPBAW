f46b0e43472014b19a2092de90e23221343fd32b
Kacper <kacper.jerzy.kawala@gmail.com>
2022-05-22 10:41:18 +0200

    alpha v2.6.4

--- app/delivery_action.php
+++ app/delivery_action.php
@@ -5,6 +5,9 @@ include("database.php");
 function delivery_action($value, $conn)
 {
   $sql = mysqli_query($conn,"SELECT 'nazwa' FROM 'items' WHERE (SELECT 'ID_ITEM' FROM 'nool' WHERE (SELECT 'NOOL' FROM 'NOOW' WHERE 'NOOW = $delivery'))");
+  echo '<ul>';
+  while($sql){echo '<li>$sql["nazwa"]</li>';}
+  echo '</ul>';
 }
 
 ?>
