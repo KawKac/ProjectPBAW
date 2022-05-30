5c8fee48519ef683061472b8e8f9c5e2c980939a
Kacper <kacper.jerzy.kawala@gmail.com>
2022-05-30 19:21:45 +0200

    alpha v3.0.10

--- app/login_action.php
+++ app/login_action.php
@@ -2,20 +2,22 @@
 	include('database.php');
 	$login = $_POST['login'];
 	$pass = md5($_POST['password']);
-	$opposit_of_login=mysqli_query($conn,"SELECT `HASLO` FROM `users` WHERE `LOGIN=$login`");
-	echo $opposit_of_login;
-	if($pass==$opposit_of_login)
+	$opposit_of_login=mysqli_fetch_array(mysqli_query($conn,"SELECT `HASLO` FROM `users` WHERE `LOGIN` LIKE '$login'"));
+	if($pass==$opposit_of_login['HASLO'])
 	{
-		$chmode=mysqli_query($conn,"SELECT `ID_CHMODE` FROM `user_chmode` WHERE `ID_USER`=$login");
-		echo $chmode;
-		setcookie('chmode','$chmode',time()+1800);
-		setcookie('user','$login',time()+1800);
+		$chmode=mysqli_fetch_array(mysqli_query($conn,"SELECT `ID_CHMODE` FROM `users_chmode` WHERE `ID_USERS` LIKE '$login'"));
+		setcookie("chmode",null);
+		setcookie('user',null);
+		setcookie('info',null);
+		setcookie("chmode",intval($chmode['ID_CHMODE']),time()+1800);
+		setcookie('user',$login,time()+1800);
 		setcookie('info','0',time()+1800);
-		print_r[$_COOKIE];
+		print_r($_COOKIE);
 		session_start();
-		//header("Location: ../");
+		header("Location: ../");
 	} else {
 		setcookie('info','1',time()+1800);
-		//header("Location: ../");
+		header("Location: ../");
 	}
+	mysqli_close($conn);
 ?>
