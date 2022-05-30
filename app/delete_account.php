05dab01f29e0a92a77a831549ec83084084f076b
Kacper <kacper.jerzy.kawala@gmail.com>
2022-05-30 22:52:24 +0200

    beta v0.0.3

--- app/delete_account.php
+++ app/delete_account.php
@@ -2,5 +2,6 @@
 include('database.php');
 $login=$_COOKIE['user'];
 mysqli_query($conn,"DELETE FROM `users` WHERE `LOGIN`='$login'");
+mysqli_close($conn);
 header('Location: app/logout.php');
 ?>
--- app/option_action.php
+++ app/option_action.php
@@ -1,7 +1,7 @@
 <?php
 include('database.php');
 $login=$_COOKIE['user'];
-$i=0;
+$i=intval('0');
 if(isset($_POST['password'])){$sql[$i]="UPDATE `users` SET `HASLO` = \'".md5($_POST['password'])."\' WHERE `LOGIN`='$login'";$i++;}
 elseif(isset($_POST['telefon'])){$sql[$i]="UPDATE `users` SET `TELEFON` = \'".$_POST['telefon']."\' WHERE `LOGIN`='$login'";$i++;}
 elseif(isset($_POST['email'])){$sql[$i]="UPDATE `users` SET `E_MAIL` = \'".$_POST['email']."\' WHERE `LOGIN`='$login'";$i++;}
@@ -10,8 +10,10 @@ elseif(isset($_POST['budynek'])){$sql[$i]="UPDATE `users` SET `NUMER` = \'".$_PO
 elseif(isset($_POST['mieszkania'])){$sql[$i]="UPDATE `users` SET `MIESZKANIA` = \'".$_POST['mieszkania']."\' WHERE `LOGIN`='$login'";$i++;}
 elseif(isset($_POST['miasto'])){$sql[$i]="UPDATE `users` SET `MIASTO` = \'".$_POST['miasto']."\' WHERE `LOGIN`='$login'";$i++;}
 elseif(isset($_POST['kp'])){$sql[$i]="UPDATE `users` SET `KOD` = \'".$_POST['kp']."' WHERE `LOGIN`='$login'";$i++;}
-elseif(isset($_POST['panstwo'])){$sql[$i]="UPDATE `users` SET `PANSTWO` =\'".$_POST['panstwo']"' WHERE `LOGIN`='$login'";}
-while($sql)
-  mysqli_query($conn,$sql);
+elseif(isset($_POST['panstwo'])){$sql[$i]="UPDATE `users` SET `PANSTWO` =\'".$_POST['panstwo']"' WHERE `LOGIN`='$login'";$i++;}
+else $sql[$i]=NULL;
+$i=intval('0');
+while($sql[$i])
+  {mysqli_query($conn,$sql[$i]);$i++;}
 mysqli_close($conn);
  ?>
