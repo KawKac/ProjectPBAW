f197b19f525112f2043fce9745c4d8c4594e1e2c
Kacper <kacper.jerzy.kawala@gmail.com>
2022-06-02 18:21:06 +0200

    beta v1.7.2

--- app/admintools.php
+++ app/admintools.php
@@ -3,7 +3,6 @@ include('database.php');
 if($_COOKIE['chmode']!=3)
   echo "Access denied!";
 else { ?>
-<form class="" action="index.html" method="post">
   <table>
     <tr>
       <th class="edit">IMIĘ</th>
@@ -12,16 +11,17 @@ else { ?>
       <th class="edit">E-MAIL</th>
       <th class="edit">TELEFON</th>
       <th class="edit">USER</th>
-      <th class="edit">PRACOWNIK</th>
-      <th class="edit">ADMINISTRACJA</th>
+      <th class="edit">EMPLOY</th>
+      <th class="edit">ADMIN</th>
       <th class="edit">EDYTUJ</th>
-      <th class="edit">USUŃ</th>
+      <th class="edit">DEZAKTYWUJ</th>
     </tr>
 <?php
     $sql="SELECT `users`.`IMIE`, `users`.`NAZWISKO`, `users`.`LOGIN`, `users`.`E_MAIL`, `users`.`TELEFON`, `users_chmode`.`ID_CHMODE` FROM `users`, `users_chmode` WHERE `users`.`LOGIN` LIKE `users_chmode`.`ID_USERS`";
     $wynik = mysqli_query($conn,$sql);
     while($row = mysqli_fetch_row($wynik)) {
       echo "<tr>";
+      echo '<form class="" action="index.html" method="post">';
       echo "<td>".$row[0]."</td>";
       echo "<td>".$row[1]."</td>";
       echo "<td>".$row[2]."</td>";
@@ -29,26 +29,26 @@ else { ?>
       echo "<td>".$row[4]."</td>";
       switch ($row[5]) {
         case 1:
-          echo "<td><input type='checkbox' name='user' heigh='20px' width='20px' checked></td>";
-          echo "<td><input type='checkbox' name='employ' heigh='20px' width='20px'></td>";
-          echo "<td><input type='checkbox' name='admin' heigh='20px' width='20px'></td>";
+          echo "<td><input type='checkbox' name='user' checked class='checkboks' id='user'><label for='user'></label></td>";
+          echo "<td><input type='checkbox' name='employ' class='checkboks' id='employ'><label for='employ'></label></td>";
+          echo "<td><input type='checkbox' name='admin' class='checkboks' id='admin'><label for='admin'></label></td>";
           break;
         case 2:
-          echo "<td><input type='checkbox' name='user' heigh='20px' width='20px'></td>";
-          echo "<td><input type='checkbox' name='employ' heigh='20px' width='20px' checked></td>";
-          echo "<td><input type='checkbox' name='admin' heigh='20px' width='20px'></td>";
+          echo "<td><input type='checkbox' name='user' class='checkboks' id='user'><label for='user'></label></td>";
+          echo "<td><input type='checkbox' name='employ' checked class='checkboks' id='employ'><label for='employ'></label></td>";
+          echo "<td><input type='checkbox' name='admin' class='checkboks' id='admin'><label for='admin'></label></td>";
           break;
         case 3:
-          echo "<td><input type='checkbox' name='user' heigh='20px' width='20px'></td>";
-          echo "<td><input type='checkbox' name='employ' heigh='20px' width='20px'></td>";
-          echo "<td><input type='checkbox' heigh='20px' width='20px' checked name='admin'></td>";
+          echo "<td><input type='checkbox' name='user' class='checkboks' id='user'><label for='user'></label></td>";
+          echo "<td><input type='checkbox' name='employ' class='checkboks' id='employ'><label for='employ'></label></td>";
+          echo "<td><input type='checkbox' checked name='admin' class='checkboks' id='admin'><label for='admin'></label></td>";
           break;
       }
-      echo "</tr>";
+      echo '<td><input type="submit" name="submit" value="zapisz"></td>';
+      echo '<td><input type="submit" name="button" value="dezaktywuj"></td>';
+      echo "</form></tr>";
     }
   echo '
   </table>
   <br>
-  <input type="submit" name="" value="zapisz">
-</form>
   ';}?>
