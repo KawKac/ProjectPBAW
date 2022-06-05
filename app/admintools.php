215c169b95efed9fd0da0b7a16493bbe091f5bbe
Kacper <kacper.jerzy.kawala@gmail.com>
2022-06-05 15:03:35 +0200

    beta v1.7.4

--- app/admintools.php
+++ app/admintools.php
@@ -3,6 +3,7 @@ include('database.php');
 if($_COOKIE['chmode']!=3)
   echo "Access denied!";
 else { ?>
+  <form action="app/admintools_action.php" method="post">
   <table>
     <tr>
       <th class="edit">IMIĘ</th>
@@ -13,44 +14,41 @@ else { ?>
       <th class="edit">USER</th>
       <th class="edit">EMPLOY</th>
       <th class="edit">ADMIN</th>
-      <th class="edit">EDYTUJ</th>
       <th class="edit">DEZAKTYWUJ</th>
     </tr>
 <?php
     $sql="SELECT `users`.`IMIE`, `users`.`NAZWISKO`, `users`.`LOGIN`, `users`.`E_MAIL`, `users`.`TELEFON`, `users_chmode`.`ID_CHMODE` FROM `users`, `users_chmode` WHERE `users`.`LOGIN` LIKE `users_chmode`.`ID_USERS`";
     $wynik = mysqli_query($conn,$sql);
     $i = 0;
+    $arr = mysqli_fetch_array($wynik);
     while($row = mysqli_fetch_row($wynik)) {
       echo "<tr>";
-      echo '<form class="" action="admintools_action.php" method="post">';
-      echo "<td class='edit'><div id='name'>".$row[0]."</div></td>";
-      echo "<td class='edit'><div id='vorname'>".$row[1]."</div></td>";
-      echo "<td class='edit'><div id='log'>".$row[2]."</div></td>";
-      echo "<td class='edit'><div id='email'>".$row[3]."</div></td>";
-      echo "<td class='edit'><div id='tel'>".$row[4]."</div></td>";
+      echo "<td class='edit'>".$row[0]."</td>";
+      echo "<td class='edit'>".$row[1]."</td>";
+      echo "<td class='edit'>".$row[2]."</td>";
+      echo "<td class='edit'>".$row[3]."</td>";
+      echo "<td class='edit'>".$row[4]."</td>";
       switch ($row[5]) {
         case 1:
-          echo "<td class='edit'><input type='radio' name='chmode".$i."' checked class='checkboks' id='user".$i."'><label for='user".$i."'></label></td>";
-          echo "<td class='edit'><input type='radio' name='chmode".$i."' class='checkboks' id='employ".$i."'><label for='employ".$i."'></label></td>";
-          echo "<td class='edit'><input type='radio' name='chmode".$i."' class='checkboks' id='admin".$i."'><label for='admin".$i."'></label></td>";
+          echo "<td class='edit'><a href='{url action=}/$arr[$i]}'>USER</a></td>";
+          echo "<td class='edit'><a href='{url action=}/$arr[$i]}'>EMPLOY</a></td>";
+          echo "<td class='edit'><a href='{url action=}/$arr[$i]}'>ADMIN</a></td>";
           break;
         case 2:
-          echo "<td class='edit'><input type='radio' name='chmode".$i."' class='checkboks' id='user".$i."'><label for='user".$i."'></label></td>";
-          echo "<td class='edit'><input type='radio' name='chmode".$i."' checked class='checkboks' id='employ".$i."'><label for='employ".$i."'></label></td>";
-          echo "<td class='edit'><input type='radio' name='chmode".$i."' class='checkboks' id='admin".$i."'><label for='admin".$i."'></label></td>";
+          echo "<td class='edit'><a href='{url action=}/$arr[$i]}'>USER</a></td>";
+          echo "<td class='edit'><a href='{url action=}/$arr[$i]}'>EMPLOY</a></td>";
+          echo "<td class='edit'><a href='{url action=}/$arr[$i]}'>ADMIN</a></td>";
           break;
         case 3:
-          echo "<td class='edit'><input type='radio' name='chmode".$i."' class='checkboks' id='user".$i."'><label for='user".$i."'></label></td>";
-          echo "<td class='edit'><input type='radio' name='chmode".$i."' class='checkboks' id='employ".$i."'><label for='employ".$i."'></label></td>";
-          echo "<td class='edit'><input type='radio' checked name='chmode".$i."' class='checkboks' id='admin".$i."'><label for='admin".$i."'></label></td>";
+          echo "<td class='edit'><a href='{}/$arr[$i]}'>USER</a></td>";
+          echo "<td class='edit'><a href='{}/$arr[$i]}'>EMPLOY</a></td>";
+          echo "<td class='edit'><a href='{}/$arr[$i]}'>ADMIN</a></td>";
           break;
       }
-      echo '<td class="edit"><input type="submit" name="submit" value="zapisz"></td>';
-      echo '<td class="edit"><input type="submit" name="button" value="dezaktywuj"></td>';
-      echo "</form></tr>";
-      $i++;
+      echo "<td class='edit'><a href='{}/$arr[$i]}'>ADMIN</a></td>";
+      echo "</tr>";
     }
   echo '
   </table>
-  <br>
+  </form>
   ';}?>
--- app/admintools_action.php
+++ app/admintools_action.php
@@ -1,5 +1,8 @@
 <?php
-$
+if($_COOKIE['chmode']!=3)
+  echo "Access denied!";
+else {
+  $wynik = $_COOKIE['wynik'];
 
-
- ?>
+}
+?>
