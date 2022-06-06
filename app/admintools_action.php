f57e503bcd006a2e633f5fb1c04a3b067ba9ad6f
Kacper <kacper.jerzy.kawala@gmail.com>
2022-06-06 10:45:07 +0200

    v0.0.2

--- app/admintools_action.php
+++ app/admintools_action.php
@@ -11,23 +11,23 @@ else {
     $chmode=$_GET['chmode'];
     $sql = "UPDATE `users_chmode` SET `ID_CHMODE`='$chmode' WHERE `ID_USERS`='$login'";
     mysqli_query($conn,$sql);
-    setcookie('message',"<div class='alert alert-success' role='alert'>Update użytkownika: <b>".$login."</b> przeprowadzono pomyślnie</div>", time()+2,'/');
+    setcookie('message',"<div class='alert alert-success' role='alert'>Update użytkownika: <b>".$login."</b> przeprowadzono pomyślnie</div>", time()+1,'/');
   }
   else
   {
-    setcookie('message',"<div class='alert alert-danger' role='alert'>Nie udało zmienić się uprawnień</div>", time()+2,'/');
+    setcookie('message',"<div class='alert alert-danger' role='alert'>Nie udało zmienić się uprawnień</div>", time()+1,'/');
   }
   if(!empty($_GET['dezaktywuj']))
   {
     $dezaktywuj=intval($_GET['dezaktywuj']);
     $sql = "UPDATE `users` SET `AKTYWNE`='$dezaktywuj' WHERE `LOGIN`='$login'";
-    mysqli_query($conn,$sql);
-    setcookie('message',"<div class='alert alert-success' role='alert'>Update użytkownika: <b>".$login."</b> przeprowadzono pomyślnie</div>", time()+2,'/');
+    mysqli_query($conn,$sql) or die($mysqli_error($conn));
+    setcookie('message',"<div class='alert alert-success' role='alert'>Update użytkownika: <b>".$login."</b> przeprowadzono pomyślnie</div>", time()+1,'/');
   }
   else {
-    setcookie('message',"<div class='alert alert-danger' role='alert'>Nie udało się (dez-)aktywować konta</div>", time()+2,'/');
+    setcookie('message',"<div class='alert alert-danger' role='alert'>Nie udało się (dez-)aktywować konta</div>", time()+1,'/');
   }
-  setcookie('info','8',time()+2,'/');
+  setcookie('info','8',time()+1,'/');
   header("Location: ../");
 }
 ?>
