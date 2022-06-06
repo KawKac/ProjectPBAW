82585f768274b9b45c96a9d0045915d00c932df8
Kacper <kacper.jerzy.kawala@gmail.com>
2022-06-06 09:18:20 +0200

    beta v1.8.3

--- app/twojezamowienia.php
+++ app/twojezamowienia.php
@@ -6,9 +6,42 @@ if($_SESSION['chmode']!='1')
   echo 'Access denied!';
 }
 else {
+  ?><section class="content_page"><?php
   $login = $_SESSION['user'];
-  $sql = "SELECT `orders`.`ID_NOOW`,`noow`.`NOOL` FROM `orders`,`noow` WHERE `noow`.`NOOW` LIKE `orders`.`ID_NOOW` AND `orders`.`ID_USER`='$login'";
-  $arr=mysqli_query($conn,$sql);
-  
+  $sql = "SELECT `ID_NOOW`, `STATUS`,`WYSYLKA` FROM `noow` WHERE `ID_USER` LIKE '$login'";
+  $wynik=mysqli_query($conn,$sql);
+  if(!empty($wynik))
+  {
+    ?>
+    <table>
+      <tr>
+        <th>NUMER PRZESYŁKI</th>
+        <th>STATUS</th>
+        <th>DATA WYSŁANIA</th>
+      </tr>
+    <?php
+    while($row = mysqli_fetch_row($wynik))
+    {
+      echo "<tr>";
+        echo "<td>";
+          echo $row[0];
+        echo "</td>";
+        echo "<td>";
+          echo $row[1];
+        echo "</td>";
+        echo "<td>";
+          echo $row[2];
+        echo "</td>";
+      echo "</tr>";
+    }
+    ?>
+    </table>
+    <?php
+  }
+  else
+  {
+      echo "<h2>BRAK ZAMÓWIEŃ</h2>";
+  }
+  ?></section><?php
 }
  ?>
